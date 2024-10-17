d5865b8b60f5ff854ff46d910c5636c1d5612124
Huong Giang <giangnth04work@gmail.com>
2024-10-17 16:43:52 +0700

    news page

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,18 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from "./redux/store.js";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <Provider store={store}>
-       <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import serviceReducer from './features/serviceSlice';
 import tourReducer from './features/tourSlice';
+import newsReducer from './features/newsSlice';
+import featured_locationReducer from './features/newsSlice';
 
 const store = configureStore({
   reducer: {
     services: serviceReducer,
     tours: tourReducer,
+    news: newsReducer,
+    featured_location: featured_locationReducer,
   },
 });
 
