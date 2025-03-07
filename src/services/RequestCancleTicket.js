c7a63b52d6f4d7febb36b3031d98e7442e7f48a0
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:18:09 +0700

    Update RequestCancleTicket.js

--- src/services/RequestCancleTicket.js
+++ src/services/RequestCancleTicket.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const Update_StatusCancelTicketsByClient = async (id) => {
-  const response = await axios.post(`http://localhost:3001/Ticket/Update_StatusCancelTicketsByClient/${id}`,{}, { withCredentials: true});
+  const response = await axios.post(`https://bookingtravel-44jm.onrender.com/Ticket/Update_StatusCancelTicketsByClient/${id}`,{}, { withCredentials: true});
   return response
 };
 
