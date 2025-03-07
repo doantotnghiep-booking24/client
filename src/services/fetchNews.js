37b6f6b418281d0b39bb20b2f77803a773e9bbb2
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:20:30 +0700

    Update fetchNews.js

--- src/services/fetchNews.js
+++ src/services/fetchNews.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchNewsData = async () => {
-  const response = await axios.get('http://localhost:3001/News/GetAllNews',{ withCredentials: true});
+  const response = await axios.get('https://bookingtravel-44jm.onrender.com/News/GetAllNews',{ withCredentials: true});
   return response.data.News;
 };
