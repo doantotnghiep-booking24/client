e315874c412c9daefed172a1e8ac31e0054c72e5
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:18:59 +0700

    Update fetchCategory.js

--- src/services/fetchCategory.js
+++ src/services/fetchCategory.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchCategories = async () => {
-  const response = await axios.get('http://localhost:3001/V2/Category/getCategories',{ withCredentials: true});
+  const response = await axios.get('https://bookingtravel-44jm.onrender.com/V2/Category/getCategories',{ withCredentials: true});
   const result = response.data.Categories.filter(t => t.isDeleted === false);
   return result;
 };
