14f969cc8be8decf0351880c57b36f706a49bdbf
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:16:25 +0700

    Update CreatePayment_Direct.js

--- src/services/CreatePayment_Direct.js
+++ src/services/CreatePayment_Direct.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const CreatePayment_Direct = async (data) => {
-  const response = await axios.post('https://bookingtravel-jhcv.onrender.com/Ticket/Direct_PaymentTicket',data, { withCredentials: true});
+  const response = await axios.post('https://bookingtravel-44jm.onrender.com/Ticket/Direct_PaymentTicket',data, { withCredentials: true});
   return response
 };
 
