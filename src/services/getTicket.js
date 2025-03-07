0ef370f28f1c03bf43b2ed9a9d4c28081f5b4fa4
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:22:09 +0700

    Update getTicket.js

--- src/services/getTicket.js
+++ src/services/getTicket.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const getTicket = async (id) => {
-  const response = await axios.post(`http://localhost:3001/Ticket/FindTicket/${id}`, {}, { withCredentials: true });
+  const response = await axios.post(`https://bookingtravel-44jm.onrender.com/Ticket/FindTicket/${id}`, {}, { withCredentials: true });
   return response
 
 };
