15d338ba89f28f247816a3927a230e8489511a38
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:16:58 +0700

    Update CreateUrlZalopay.js

--- src/services/CreateUrlZalopay.js
+++ src/services/CreateUrlZalopay.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const CreateUrlZalopay = async (data) => {
-  const response = await axios.post('https://bookingtravel-jhcv.onrender.com/Ticket/PaymentZalopay',data,{ withCredentials: true});
+  const response = await axios.post('https://bookingtravel-44jm.onrender.com/Ticket/PaymentZalopay',data,{ withCredentials: true});
   return response
 };
 
