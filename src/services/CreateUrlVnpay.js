afe80ce3bdd5d8ff571d6703b3acd806669d2218
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:16:41 +0700

    Update CreateUrlVnpay.js

--- src/services/CreateUrlVnpay.js
+++ src/services/CreateUrlVnpay.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const CreateUrlVnpay = async (data) => {
-  const response = await axios.post('https://bookingtravel-jhcv.onrender.com/Ticket/create_payment_url', data, { withCredentials: true });
+  const response = await axios.post('https://bookingtravel-44jm.onrender.com/Ticket/create_payment_url', data, { withCredentials: true });
   return response
 };
 
