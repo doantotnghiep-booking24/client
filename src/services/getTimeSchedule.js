680ec9a2a18254fd39be0e0ca7ba0a50d4bfe0a1
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:22:45 +0700

    Update getTimeSchedule.js

--- src/services/getTimeSchedule.js
+++ src/services/getTimeSchedule.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const GetAllTimeSchedule = async () => {
-  const response = await axios.get('http://localhost:3001/TimeSchedules/getTimeSchedules',{ withCredentials: true});
+  const response = await axios.get('https://bookingtravel-44jm.onrender.com/TimeSchedules/getTimeSchedules',{ withCredentials: true});
   return response.data
 };
 
