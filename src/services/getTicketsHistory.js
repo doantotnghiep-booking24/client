8ff7ecb4a8e8988ce769fa137ae8517bd9b805d4
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:22:30 +0700

    Update getTicketsHistory.js

--- src/services/getTicketsHistory.js
+++ src/services/getTicketsHistory.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const GetAllTicket = async () => {
-  const response = await axios.get('http://localhost:3001/Ticket/GetAllTicket',{ withCredentials: true});
+  const response = await axios.get('https://bookingtravel-44jm.onrender.com/Ticket/GetAllTicket',{ withCredentials: true});
   return response
 };
 
