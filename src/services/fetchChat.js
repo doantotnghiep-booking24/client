8872cdb959b7f2d1935515ed77e3a891c7303e2e
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:19:22 +0700

    Update fetchChat.js

--- src/services/fetchChat.js
+++ src/services/fetchChat.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 export const fetchChat = async (userId) => {
-  const response = await axios.get(`http://localhost:3001/chat/${userId}`, {withCredentials: true});
+  const response = await axios.get(`https://bookingtravel-44jm.onrender.com/chat/${userId}`, {withCredentials: true});
   return response.data;
 };
 
 export const fetchAllChat = async () => {
-  const response = await axios.get(`http://localhost:3001/chat/`, {withCredentials: true});
+  const response = await axios.get(`https://bookingtravel-44jm.onrender.com/chat/`, {withCredentials: true});
   // console.log(response.data);
   return response.data;
 };
 
 export const fetchChatByIdUser = async (userId) => {
-  const response = await axios.get(`http://localhost:3001/chat/chats/${userId}`, {withCredentials: true});
+  const response = await axios.get(`https://bookingtravel-44jm.onrender.com/chat/chats/${userId}`, {withCredentials: true});
   console.log(response.data);
   return response.data;
 
