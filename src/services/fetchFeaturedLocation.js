f7528d6c2418b9260b6b3347873def1c8378032f
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-07 21:20:00 +0700

    Update fetchFeaturedLocation.js

--- src/services/fetchFeaturedLocation.js
+++ src/services/fetchFeaturedLocation.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchFeaturedLocationData = async () => {
-  const response = await axios.get('http://localhost:3001/V2/Featured_Location/GetFeatured_Location',{ withCredentials: true});
+  const response = await axios.get('https://bookingtravel-44jm.onrender.com/V2/Featured_Location/GetFeatured_Location',{ withCredentials: true});
   return response.data.Featured_Location; 
 };
