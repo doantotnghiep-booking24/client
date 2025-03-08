97c8aa3eada33cc034c3410a348f709ba7bdb805
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-08 13:44:45 +0700

    Update fetchTours.js

--- src/services/fetchTours.js
+++ src/services/fetchTours.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchToursData = async () => {
 
-  const response = await axios.get('https://bookingtravel-44jm.onrender.com/V1/Tours', { withCredentials: true });
+  const response = await axios.get('https://bookingtravel-44jm.onrender.com', { withCredentials: true });
   const result = response.data.Tours.datas.filter(t => t.isDeleted === false);
 // console.log(result);
 
