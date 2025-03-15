bab53abd5c3a6ee95182777e5b6695e27bbce145
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-15 13:11:39 +0700

    Update GetSchedule_Travel.js

--- src/services/GetSchedule_Travel.js
+++ src/services/GetSchedule_Travel.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getScheduleByid = async (id) => {
-  const response = await axios.get(`https://bookingtravel-44jm.onrender.com/GetSchedule/${id}`, { withCredentials: true });
+  const response = await axios.get(`https://bookingtravel-44jm.onrender.com/Schedules/GetSchedule/${id}`, { withCredentials: true });
   return response.data;
 };
 
